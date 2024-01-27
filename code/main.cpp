ba207b6490a0ca6dd72e939907abafad98eff3f8
Yannick Hradetzky <hradetzky.y@gmail.com>
2024-01-27 09:04:32 +0100

    1

--- code/main.cpp
+++ code/main.cpp
@@ -91,65 +91,71 @@ int main() {
         }
     }
 
-    // for(int n = 10; n <= nMax; n *= 10 ){
-    //     cout << "Running for n = " << n << endl;
+    for(int n = 10; n <= nMax; n *= 10 ){
+        cout << "Running for n = " << n << endl;
         
-    //     for (double Density = 0.1; Density <= 10; Density += 0.1){
-    //         while (true) {
-    //             if (threads.size() <= num_cores) {
-    //                 threads.emplace_back([n, Density, &globalMutex]() {
-    //                     // Create a new Simulation object for each thread
-    //                     Simulation sim;
-    //                     sim.NParticles = n;
-    //                     sim.DensityInit = Density;
-    //                     sim.NStepsSimulation = NSim;
-    //                     sim.NStepsEquilibration = NEquil;
-    //                     sim.NStepsSampling = NSamp;
-    //                     sim.TimeStepSize = TimeStepSize;
-    //                     sim.PrintInitInfo = false;
-    //                     sim.PrintProgress = false;
+        for (double Density = 0.1; Density <= 10; Density += 0.1){
+            while (true) {
+                if (threads.size() <= num_cores) {
+                    threads.emplace_back([n, Density, &globalMutex]() {
+                        // Create a new Simulation object for each thread
+                        Simulation sim;
+                        sim.NParticles = n;
+                        sim.DensityInit = Density;
+                        sim.NStepsSimulation = NSim;
+                        sim.NStepsEquilibration = NEquil;
+                        sim.NStepsSampling = NSamp;
+                        sim.TimeStepSize = TimeStepSize;
+                        sim.PrintInitInfo = false;
+                        sim.PrintProgress = false;
 
-    //                     string filenameDensity = "out/vicsek/" + to_string(sim.NParticles) + "/Density_" + to_string(Density) + ".txt";
+                        string filenameDensity = "out/vicsek/" + to_string(sim.NParticles) + "/Density_" + to_string(Density) + ".txt";
 
-    //                     // Open the file
-    //                     ofstream ResultsFile(filenameDensity);
+                        // Open the file
+                        ofstream ResultsFile(filenameDensity);
 
-    //                     // For each Density value, run the simulation multiple times
-    //                     for (int i = 1; i <= times; ++i) {
-    //                         // Use unique seed for each thread
-    //                         srand(i * static_cast<unsigned int>(hash<thread::id>{}(this_thread::get_id())));
+                        // For each Density value, run the simulation multiple times
+                        for (int i = 1; i <= times; ++i) {
+                            // Use unique seed for each thread
+                            srand(i * static_cast<unsigned int>(hash<thread::id>{}(this_thread::get_id())));
 
-    //                         // Run the simulation for a given density
-    //                         vector<double> Results = sim.RunVicsekForDensity(Density, NoiseForDensity);
+                            // Run the simulation for a given density
+                            vector<double> Results = sim.RunVicsekForDensity(Density, NoiseForDensity);
 
-    //                         // Use the global mutex to synchronize file writing
-    //                         lock_guard<mutex> lock(globalMutex);
+                            // Use the global mutex to synchronize file writing
+                            lock_guard<mutex> lock(globalMutex);
 
-    //                         // Write the results to a file
-    //                         for (int j = 0; j < Results.size(); ++j) {
-    //                             ResultsFile << Results[j] << " ";
-    //                         }
-    //                         ResultsFile << endl;
-    //                     }
-    //                     ResultsFile.close();
-    //                     cout << "Finished Density = " << Density << endl;
-    //                 });
-    //                 break;
-    //             }
-    //             else {
-    //                 // Wait for any thread to finish, then join and erase it
-    //                 for (auto it = threads.begin(); it != threads.end(); ++it) {
-    //                     if (it->joinable()) {
-    //                         it->join();
-    //                         threads.erase(it);
-    //                         break;
-    //                     }
-    //                 }
-    //             }
-    //         }
-    //     }
-    // }
+                            // Write the results to a file
+                            for (int j = 0; j < Results.size(); ++j) {
+                                ResultsFile << Results[j] << " ";
+                            }
+                            ResultsFile << endl;
+                        }
+                        ResultsFile.close();
+                        cout << "Finished Density = " << Density << endl;
+                    });
+                    break;
+                }
+                else {
+                    // Wait for any thread to finish, then join and erase it
+                    for (auto it = threads.begin(); it != threads.end(); ++it) {
+                        if (it->joinable()) {
+                            it->join();
+                            threads.erase(it);
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+    }
 
+
+    for (double Noise = 0; Noise <= 2 * M_PI; Noise += 0.1) {
+            while (true) {
+                if (threads.size() <= num_cores) {
+                    threads.emplace_back([n, Noise, &globalMutex]() {
+                        
     // Join remaining threads
     for (auto &t : threads) {
         if (t.joinable()) {
